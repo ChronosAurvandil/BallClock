dd88f863e83be222b314580a7cec964890e32032
Justin T. Berger <justinthomasberger@gmail.com>
2014-06-09 23:14:52 -0600

    'Hello World' status

--- BallClock.rb
+++ BallClock.rb
@@ -1,4 +1,8 @@
 # Justin Berger
 class BallClock
+  attr_reader :Time
   
+  def initialize
+    @Time = "1:00PM";
+  end
 end
--- Tester.rb
+++ Tester.rb
@@ -1,4 +1,11 @@
 # Justin Berger
 class Tester
-  
+  def Test
+    require '.\BallClock.rb'
+    clock = BallClock.new
+    print clock.Time
+  end
 end
+
+t = Tester.new
+t.Test
